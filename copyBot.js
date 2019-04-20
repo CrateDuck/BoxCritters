969883deef4c2d809fad8074d3fe7eb035ddb048
CrateDuck <49816699+CrateDuck@users.noreply.github.com>
2019-04-20 14:41:25 -0500

    Update copyBot.js

--- copyBot.js
+++ copyBot.js
@@ -2,7 +2,7 @@ console.defaultLog = console.log.bind(console);
 console.info = function() {
     console.defaultLog.apply(console, arguments);
     let e = Array.from(arguments);
-    if(e[0] == "M") {
+    if(e[0] == "M" && e[1].n != world.player.nickname) {
 	world.sendMessage(e[1].m);
     }
 }
