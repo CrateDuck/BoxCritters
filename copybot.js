67d580b943aed25c6ea34acb76b6e1e30e77254f
CrateDuck <49816699+CrateDuck@users.noreply.github.com>
2019-04-20 10:58:57 -0500

    Create copybot.js

---
+++ copybot.js
@@ -0,0 +1,14 @@
+/*
+
+Copy Bot
+programmed by CrateDuck
+
+/dab
+
+*/
+console.stdlog = console.log.bind(console);
+console.logs = [];
+console.log = function(){
+    console.logs.push(Array.from(arguments));
+    console.stdlog.apply(console, arguments);
+}
