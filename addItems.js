9b187a857e3d67a08e996eaa1f4d0cd39d781bcb
CrateDuck <49816699+CrateDuck@users.noreply.github.com>
2019-04-20 13:11:03 -0500

    Update and rename copybot.js to addItems.js

---
+++ addItems.js
@@ -0,0 +1,598 @@
+let animations =  {
+    backpack_green_BACK0: {
+        frames: [0]
+    },
+    backpack_green_BACK4: {
+        frames: [1]
+    },
+    backpack_green_BACK1: {
+        frames: [2]
+    },
+    backpack_green_BACK7: {
+        frames: [3]
+    },
+    backpack_green_BACK2: {
+        frames: [4]
+    },
+    backpack_green_BACK3: {
+        frames: [5]
+    },
+    backpack_green_BACK5: {
+        frames: [6]
+    },
+    backpack_green_BACK6: {
+        frames: [7]
+    },
+    bunny_blue0: {
+        frames: [8]
+    },
+    bunny_blue4: {
+        frames: [9]
+    },
+    bunny_pink0: {
+        frames: [10]
+    },
+    bunny_pink4: {
+        frames: [11]
+    },
+    bunny_white0: {
+        frames: [12]
+    },
+    bunny_white4: {
+        frames: [13]
+    },
+    bunny_blue5: {
+        frames: [14]
+    },
+    bunny_blue6: {
+        frames: [15]
+    },
+    bunny_pink2: {
+        frames: [16]
+    },
+    bunny_pink3: {
+        frames: [17]
+    },
+    bunny_pink5: {
+        frames: [18]
+    },
+    bunny_pink6: {
+        frames: [19]
+    },
+    bunny_white5: {
+        frames: [20]
+    },
+    bunny_white6: {
+        frames: [21]
+    },
+    paperhat0: {
+        frames: [22]
+    },
+    bunny_blue2: {
+        frames: [23]
+    },
+    bunny_blue3: {
+        frames: [24]
+    },
+    bunny_white2: {
+        frames: [25]
+    },
+    bunny_white3: {
+        frames: [26]
+    },
+    bunny_blue1: {
+        frames: [27]
+    },
+    bunny_pink7: {
+        frames: [28]
+    },
+    bunny_white1: {
+        frames: [29]
+    },
+    bunny_blue7: {
+        frames: [30]
+    },
+    bunny_pink1: {
+        frames: [31]
+    },
+    bunny_white7: {
+        frames: [32]
+    },
+    easteregg_c0: {
+        frames: [33]
+    },
+    easteregg_c1: {
+        frames: [34]
+    },
+    easteregg_c7: {
+        frames: [35]
+    },
+    sleeping0: {
+        frames: [36]
+    },
+    sleeping4: {
+        frames: [37]
+    },
+    sleeping2: {
+        frames: [38]
+    },
+    sleeping3: {
+        frames: [39]
+    },
+    easteregg_a2: {
+        frames: [40]
+    },
+    easteregg_a3: {
+        frames: [41]
+    },
+    easteregg_a4: {
+        frames: [42]
+    },
+    easteregg_a5: {
+        frames: [43]
+    },
+    easteregg_a6: {
+        frames: [44]
+    },
+    easteregg_a0: {
+        frames: [45]
+    },
+    easteregg_a1: {
+        frames: [46]
+    },
+    easteregg_a7: {
+        frames: [47]
+    },
+    sleeping7: {
+        frames: [48]
+    },
+    paperhat4: {
+        frames: [49]
+    },
+    headphones_white4: {
+        frames: [50]
+    },
+    sleeping1: {
+        frames: [51]
+    },
+    easteregg_c2: {
+        frames: [52]
+    },
+    easteregg_c3: {
+        frames: [53]
+    },
+    easteregg_c4: {
+        frames: [54]
+    },
+    easteregg_c5: {
+        frames: [55]
+    },
+    easteregg_c6: {
+        frames: [56]
+    },
+    sleeping5: {
+        frames: [57]
+    },
+    sleeping6: {
+        frames: [58]
+    },
+    hoodie_blue2: {
+        frames: [59]
+    },
+    hoodie_blue3: {
+        frames: [60]
+    },
+    toque_white2: {
+        frames: [61]
+    },
+    toque_white3: {
+        frames: [62]
+    },
+    headphones_white2: {
+        frames: [63]
+    },
+    headphones_white3: {
+        frames: [64]
+    },
+    toque_white1: {
+        frames: [65]
+    },
+    toque_white7: {
+        frames: [66]
+    },
+    propeller1: {
+        frames: [67]
+    },
+    propeller0: {
+        frames: [68]
+    },
+    hoodie_blue0: {
+        frames: [69]
+    },
+    headphones_white0: {
+        frames: [70]
+    },
+    headphones_white_BACK0: {
+        frames: [71]
+    },
+    headphones_white_BACK4: {
+        frames: [72]
+    },
+    hoodie_blue5: {
+        frames: [73]
+    },
+    hoodie_blue6: {
+        frames: [74]
+    },
+    toque_blue1: {
+        frames: [75]
+    },
+    backpack_green2: {
+        frames: [76]
+    },
+    backpack_green3: {
+        frames: [77]
+    },
+    backpack_green4: {
+        frames: [78]
+    },
+    backpack_green5: {
+        frames: [79]
+    },
+    backpack_green6: {
+        frames: [80]
+    },
+    paperhat2: {
+        frames: [81]
+    },
+    paperhat3: {
+        frames: [82]
+    },
+    paperhat1: {
+        frames: [83]
+    },
+    paperhat5: {
+        frames: [84]
+    },
+    paperhat6: {
+        frames: [85]
+    },
+    paperhat7: {
+        frames: [86]
+    },
+    hoodie_blue1: {
+        frames: [87]
+    },
+    hoodie_blue4: {
+        frames: [88]
+    },
+    toque_blue7: {
+        frames: [89]
+    },
+    hoodie_blue7: {
+        frames: [90]
+    },
+    toque_pink1: {
+        frames: [91]
+    },
+    toque_blue0: {
+        frames: [92]
+    },
+    toque_blue2: {
+        frames: [93]
+    },
+    toque_blue3: {
+        frames: [94]
+    },
+    "3d_black2": {
+        frames: [95]
+    },
+    "3d_black3": {
+        frames: [96]
+    },
+    headphones_white1: {
+        frames: [97]
+    },
+    headphones_white5: {
+        frames: [98]
+    },
+    headphones_white6: {
+        frames: [99]
+    },
+    toque_pink7: {
+        frames: [100]
+    },
+    toque_blue4: {
+        frames: [101]
+    },
+    toque_pink2: {
+        frames: [102]
+    },
+    toque_pink3: {
+        frames: [103]
+    },
+    "3d_black5": {
+        frames: [104]
+    },
+    "3d_black6": {
+        frames: [105]
+    },
+    "3d_white2": {
+        frames: [106]
+    },
+    "3d_white3": {
+        frames: [107]
+    },
+    toque_white0: {
+        frames: [108]
+    },
+    propeller4: {
+        frames: [109]
+    },
+    toque_pink0: {
+        frames: [110]
+    },
+    "3d_white5": {
+        frames: [111]
+    },
+    "3d_white6": {
+        frames: [112]
+    },
+    easteregg_b0: {
+        frames: [113]
+    },
+    easteregg_b1: {
+        frames: [114]
+    },
+    easteregg_b7: {
+        frames: [115]
+    },
+    headphones_white_BACK1: {
+        frames: [116]
+    },
+    toque_blue5: {
+        frames: [117]
+    },
+    toque_blue6: {
+        frames: [118]
+    },
+    toque_pink4: {
+        frames: [119]
+    },
+    toque_white5: {
+        frames: [120]
+    },
+    toque_white6: {
+        frames: [121]
+    },
+    toque_pink5: {
+        frames: [122]
+    },
+    toque_pink6: {
+        frames: [123]
+    },
+    toque_white4: {
+        frames: [124]
+    },
+    headphones_white7: {
+        frames: [125]
+    },
+    viking0: {
+        frames: [126]
+    },
+    viking1: {
+        frames: [127]
+    },
+    pirate_capt_black0: {
+        frames: [128]
+    },
+    propeller7: {
+        frames: [129]
+    },
+    easteregg_b2: {
+        frames: [130]
+    },
+    easteregg_b3: {
+        frames: [131]
+    },
+    easteregg_b4: {
+        frames: [132]
+    },
+    easteregg_b5: {
+        frames: [133]
+    },
+    easteregg_b6: {
+        frames: [134]
+    },
+    pirate_capt_black1: {
+        frames: [135]
+    },
+    pirate_capt_black2: {
+        frames: [136]
+    },
+    pirate_capt_black3: {
+        frames: [137]
+    },
+    pirate_capt_black4: {
+        frames: [138]
+    },
+    "3d_black4": {
+        frames: [139]
+    },
+    pirate_capt_black5: {
+        frames: [140]
+    },
+    pirate_capt_black6: {
+        frames: [141]
+    },
+    pirate_capt_black7: {
+        frames: [142]
+    },
+    viking4: {
+        frames: [143]
+    },
+    viking2: {
+        frames: [144]
+    },
+    viking3: {
+        frames: [145]
+    },
+    "3d_white4": {
+        frames: [146]
+    },
+    plaid_red0: {
+        frames: [147]
+    },
+    propeller2: {
+        frames: [148]
+    },
+    propeller3: {
+        frames: [149]
+    },
+    viking5: {
+        frames: [150]
+    },
+    viking6: {
+        frames: [151]
+    },
+    plaid_red1: {
+        frames: [152]
+    },
+    plaid_red2: {
+        frames: [153]
+    },
+    plaid_red3: {
+        frames: [154]
+    },
+    plaid_red4: {
+        frames: [155]
+    },
+    plaid_red5: {
+        frames: [156]
+    },
+    plaid_red6: {
+        frames: [157]
+    },
+    viking7: {
+        frames: [158]
+    },
+    plaid_red7: {
+        frames: [159]
+    },
+    space_red0: {
+        frames: [160]
+    },
+    space_red1: {
+        frames: [161]
+    },
+    space_red2: {
+        frames: [162]
+    },
+    space_red3: {
+        frames: [163]
+    },
+    space_red4: {
+        frames: [164]
+    },
+    space_red7: {
+        frames: [165]
+    },
+    party_green2: {
+        frames: [166]
+    },
+    party_green3: {
+        frames: [167]
+    },
+    party_green5: {
+        frames: [168]
+    },
+    party_green6: {
+        frames: [169]
+    },
+    party_green1: {
+        frames: [170]
+    },
+    party_green7: {
+        frames: [171]
+    },
+    tshirt_white0: {
+        frames: [172]
+    },
+    space_red5: {
+        frames: [173]
+    },
+    space_red6: {
+        frames: [174]
+    },
+    party_green0: {
+        frames: [175]
+    },
+    headphones_white_BACK2: {
+        frames: [176]
+    },
+    headphones_white_BACK3: {
+        frames: [177]
+    },
+    tshirt_white1: {
+        frames: [178]
+    },
+    propeller5: {
+        frames: [179]
+    },
+    propeller6: {
+        frames: [180]
+    },
+    tshirt_white2: {
+        frames: [181]
+    },
+    tshirt_white3: {
+        frames: [182]
+    },
+    tshirt_white4: {
+        frames: [183]
+    },
+    party_green4: {
+        frames: [184]
+    },
+    "3d_black1": {
+        frames: [185]
+    },
+    tshirt_white5: {
+        frames: [186]
+    },
+    tshirt_white6: {
+        frames: [187]
+    },
+    tshirt_white7: {
+        frames: [188]
+    },
+    headphones_white_BACK5: {
+        frames: [189]
+    },
+    headphones_white_BACK6: {
+        frames: [190]
+    },
+    "3d_black7": {
+        frames: [191]
+    },
+    "3d_white1": {
+        frames: [192]
+    },
+    "3d_white7": {
+        frames: [193]
+    },
+    headphones_white_BACK7: {
+        frames: [194]
+    }
+};
+let iterated = [];
+let track = 0;
+Object.keys(animations).forEach((name) => {
+    let stripEnd = name.substring(0, name.length-1);
+    if(!iterated.includes(stripEnd)) {
+        iterated[iterated.length] = stripEnd;
+        world.player.inventory[track] = {itemId: stripEnd, slot: "body", name: "N/A"};
+        console.log('Adding item: ' + stripEnd);
+        track++;
+    }
+});
